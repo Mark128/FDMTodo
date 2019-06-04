6bb91d31da0a0b4ad6503f2a970798c4c3215ab5
Mark Antoniadis <Mark.Antoniadis@fdmgroup.com>
2019-06-04 11:48:14 -0400

    navbar html created - no routing yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TodoThumbnailComponent } from './components/todo-thumbnail/todo-thumbnail.component';
+import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
+import { TaskThumbnailComponent } from './components/task-thumbnail/task-thumbnail.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TodoThumbnailComponent,
+    TodoDetailComponent,
+    TaskThumbnailComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/todo-thumbnail/todo-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-thumbnail',
+  templateUrl: './todo-thumbnail.component.html',
+  styleUrls: ['./todo-thumbnail.component.css']
+})
+export class TodoThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
